9c67630eca521bbf91268eef0a88376cf11b7d36
Eetu Huisman <eh@iki.fi>
2012-03-21 23:30:08 +0200

    Initialize list of URL's in a static block

--- src/main/java/domain/FeedEntryRepository.java
+++ src/main/java/domain/FeedEntryRepository.java
@@ -1,33 +1,55 @@
 package domain;
 
-import com.google.common.collect.Ordering;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
 import com.google.common.primitives.Longs;
 import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.io.FeedException;
 import fetcher.FeedFetcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
-import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 
 @Repository
 public class FeedEntryRepository {
 
-    public static final String URL_PATTERN = "http://blogit.kauppalehti.fi/evs/aid/%d/recent/10/";
+    private static final String URL_PATTERN = "http://blogit.kauppalehti.fi/evs/aid/%d/recent/10/";
+    private static final Set<String> URLS = Sets.newHashSet();
+    private static final ContiguousSet<Integer> FROM_FOUR_TO_SEVEN = Ranges.closed(4, 7).asSet(DiscreteDomains.integers());
+
+    static {
+        // The URL's of the selected feeds have just one difference which happens to be a set of ints ranging from 4 to 7
+        for (Integer i : FROM_FOUR_TO_SEVEN) {
+            URLS.add(String.format(URL_PATTERN, i));
+        }
+    }
 
     @Inject
     private FeedFetcher feedFetcher;
 
-    public List<SyndEntry> fetchEntries() throws FeedException, IOException {
+    private Logger logger = LoggerFactory.getLogger(FeedEntryRepository.class);
+
+    /**
+     * Fetches the entries of predefined feeds and combines them to one list
+     * @return All unique entries sorted by publishing date
+     */
+    public List<SyndEntry> fetchEntries() {
         Set<SyndEntry> entries = Sets.newHashSet();
-        for (int i = 4; i < 8; i++) {
-            String url = String.format(URL_PATTERN, i);
-            entries.addAll(feedFetcher.fetchFrom(url));
+        for (String url : URLS) {
+            entries.addAll(entriesFrom(url));
         }
         return new NewestFirst().sortedCopy(entries);
+    }
+
+    private List<SyndEntry> entriesFrom(String url) {
+        try {
+            return feedFetcher.fetchFrom(url);
+        } catch (Exception e) {
+            logger.warn(String.format("Fetching feed from URL '%s' failed", url), e);
+            return Lists.newArrayList();
+        }
     }
 
     private static class NewestFirst extends Ordering<SyndEntry> {
